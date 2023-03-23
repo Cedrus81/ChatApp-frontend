f687cd4e9dcd2cba5cbe44114b580a9d224db85c
Cedrus81 <erez.eitan.97@gmail.com>
2023-03-23 11:25:23 +0200

    more work on the login options ui

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,24 +1,48 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IoMdMail, IoMdLock } from "react-icons/io";
+import { faFacebookF, faGithub, faGoogle, faTwitter } from "@fortawesome/free-brands-svg-icons";
 function Login() {
-  
+  const strategies = {
+    google:{ 
+      handler: () => console.log('use google'),
+      icon: <FontAwesomeIcon icon={faGoogle} />
+    },
+    tweeter: {
+      handler: () => console.log('use tweeter'),
+      icon: <FontAwesomeIcon icon={faTwitter} />
+    },
+    github: {
+      handler: () => console.log('use github'),
+      icon: <FontAwesomeIcon icon={faGithub} />
+    },
+    facebook: {
+      handler: () => console.log('use facebook'),
+      icon: <FontAwesomeIcon icon={faFacebookF} />
+    }
+  }
   return (
     <section className='login window'>
       <h2>Auth Wiedersehen</h2>
       <h1>Login</h1>
       <form action="">
         <div className="input-box">
-          <label htmlFor="input-email"><IoMdMail /> Email</label>
           <input id="input-email" type="email" required />
+          <label htmlFor="input-email"><IoMdMail /> Email</label>
         </div>
 
         <div className="input-box">
-          <label htmlFor="input-password"><IoMdLock /> Password</label>
           <input id="input-password" type="password" required />
+          <label htmlFor="input-password" className="input-password"><IoMdLock /> Password</label>
         </div>
         
         <button>Login</button>
       </form>
-      <p>or continue with these social profile</p>
+      <p>or continue with these social profiles</p>
+      <div className="social-strategy-list">
+        { Object.keys(strategies).map( (media, idx) => {
+          return <button key={`login-strategy-${Object.keys(strategies)[idx]}`} className="social-container" onClick={strategies[media as keyof typeof strategies].handler}>{strategies[media as keyof typeof strategies].icon}</button>
+        })}
+      </div>
       <p>Don’t have an account yet? Register</p>
     </section>
   )
