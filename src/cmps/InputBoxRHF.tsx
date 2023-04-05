420dff18426a631c0f6217ecbf92106663d6abe9
Cedrus81 <erez.eitan.97@gmail.com>
2023-04-05 13:13:37 +0300

    fully transitioned to react hook forms from formik

--- src/cmps/InputBoxRHF.tsx
+++ src/cmps/InputBoxRHF.tsx
@@ -1,27 +1,51 @@
-import { useCallback } from "react"
-import { FieldError, FieldErrorsImpl, Merge, UseFormRegisterReturn } from "react-hook-form"
+import { FieldError, UseFormRegisterReturn } from "react-hook-form"
 import { FieldData } from "../types"
+import { useMemo } from "react"
 
 type InputBoxRHFProps = {
     data: FieldData
     register: UseFormRegisterReturn<string>
     error: FieldError | (Record<string, Partial<{ type: string | number; message: string; }>> & Partial<{ type: string | number; message: string; }>) | undefined
     isDirty: boolean | undefined
+    currVal: string
 }
 
-function InputBoxRHF(props: InputBoxRHFProps) {
-    const {register, data, error, isDirty} = props
+function InputBoxRHF({register, data, error, isDirty, currVal}: InputBoxRHFProps) {
     const {id, icon, title, flexRatio, list} = data
     function shouldDisplayError(){
         return error && isDirty
     }
-  return (
-    <div className={`input-box ${flexRatio === 1 ? 'left' : flexRatio && flexRatio > 1 ? 'right' : ''}`} style={{ flex: flexRatio}}>
-        {id === 'bio' && <textarea id={id} title={title}  data-theme={`background text ${shouldDisplayError() ? 'error border' : ''}`} {...register} className={isDirty ? 'floating' : ''} ></textarea>}
-        {id !== 'bio' &&<input id={id} title={title} list={list} type={id === 'password' ? 'password' : 'text'} data-theme={`background text ${shouldDisplayError() ? 'error border' : ''}`} {...register} className={isDirty ? 'floating' : ''} />}
+    const memoizedInputBox = useMemo(()=>{
+      console.log(id, 'rendered')
+      return(
+        <div className={`input-box ${flexRatio === 1 ? 'left' : flexRatio && flexRatio > 1 ? 'right' : ''}`} style={{ flex: flexRatio}}>
+        {id === 'bio' && <textarea 
+        id={id} 
+        title={title}  
+        {...register} 
+        data-theme={`background text ${shouldDisplayError() ? 'error border' : ''}`} 
+        className={isDirty ? 'floating' : ''} >
+          </textarea>}
+        
+        {id !== 'bio' &&<input 
+        id={id} 
+        title={title} 
+        list={list} 
+        {...register} 
+        type={id === 'password' ? 'password' : 'text'} 
+        data-theme={`background text ${shouldDisplayError() ? 'error border' : ''}`} 
+        className={isDirty ? 'floating' : ''} 
+        autoComplete={id === 'password' ? 'current-password' : ''} />}
+        
         <label htmlFor={id} data-theme={`background text ${shouldDisplayError() ? ' error text' : ''}`}>{icon}{id.charAt(0).toUpperCase() + id.slice(1)}</label>
         {shouldDisplayError() && <p className="error-msg" data-theme="error text">{error?.message?.toString()}</p>}
     </div>
+      )
+    },[currVal])
+  return (
+    <>
+      {memoizedInputBox}
+    </>
 
   )
 }
--- src/views/Signup.tsx
+++ src/views/Signup.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useMemo } from "react"
-// import { useFormik } from "formik";
+import { useMemo } from "react"
 import { IoMdLock, IoMdMail, IoMdPerson, IoMdPhonePortrait, IoIosFlag } from "react-icons/io"
 import { BsFillFileEarmarkPersonFill } from "react-icons/bs";
 import { Link } from "react-router-dom"
@@ -7,11 +6,10 @@ import { countries } from "../data"
 import { signupSchema } from "../schemas";
 import { useAppDispatch } from "../hooks";
 import { FieldData } from "../types";
-import {yupResolver} from "@hookform/resolvers/yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 
 import ThemeToggle from "../cmps/ThemeToggle";
-import InputBox from "../cmps/InputBox";
 import InputBoxRHF from "../cmps/InputBoxRHF";
 
 type SignupFormValues = {
@@ -22,12 +20,12 @@ type SignupFormValues = {
     phone: string
     dial: string
 }
-
+//todo add a resetfield option (button) to each input
 function Signup() {
     const dispatch = useAppDispatch()
-    const {register, handleSubmit, formState: {errors, dirtyFields, isSubmitting}, watch, reset} = useForm<SignupFormValues>({
+    const {register, handleSubmit, formState: {errors, dirtyFields, isSubmitting}, reset, trigger, resetField, getValues} = useForm<SignupFormValues>({
         resolver: yupResolver(signupSchema),
-        defaultValues:{
+        defaultValues: {
             email: '',
             password: '',
             name: '',
@@ -36,74 +34,42 @@ function Signup() {
             dial: ''
         },
     })
-    // useEffect(() => {
-    //   const subscription = watch((data) => {
-    //     console.log(data)
-    // })
-    //   return () => {
-    //     subscription.unsubscribe()
-    //   }
-    // }, [watch])
-    // const {handleSubmit, errors, touched, isSubmitting,
-    //     values,
-    //     handleChange,
-    //     handleBlur} = useFormik({
-    //     initialValues:{
-    //         email: '',
-    //         password: '',
-    //         name:'',
-    //         bio: '',
-    //         phone: '',
-    //         dial: '',
-    //     },
-    //     validationSchema: signupSchema,
-    //     onSubmit
-    // })
-    const fields: FieldData[] = [
-       { id: 'email',
+    
+    const fields: FieldData[][] = [
+       [{ id: 'email',
         icon: <IoMdMail />,
-        },
-        {id: 'password',
+        }],
+        [{id: 'password',
          icon: <IoMdLock />,
-         title: '5-20 characters, at least 1 of each: Capital, lowercase, digit'},
-        { id: 'name',
+         title: '5-20 characters, at least 1 of each: Capital, lowercase, digit'}],
+        [{ id: 'name',
         icon: <IoMdPerson/>,
-        },
-        { id: 'bio',
+        }],
+        [
+            {
+                id: 'dial',
+                icon: <IoIosFlag />,
+                flexRatio: 1,
+                list: 'country-codes'
+            },
+            {
+                id: 'phone',
+                icon: <IoMdPhonePortrait />,
+                flexRatio: 2.5,
+                title: '10 digits length, e.g. 0541234567'
+            }
+        ],
+       [ { id: 'bio',
         icon: <BsFillFileEarmarkPersonFill />,
-        },
-    ]
-    const phoneFields: FieldData[] = [
-        {
-            id: 'dial',
-            icon: <IoIosFlag />,
-            flexRatio: 1,
-            list: 'country-codes'
-        },
-        {
-            id: 'phone',
-            icon: <IoMdPhonePortrait />,
-            flexRatio: 2.5,
-            title: '10 digits length, e.g. 0541234567'
-        }
+        }],
     ]
 
-    function onSubmit(data: any){
+    function onSubmit(data: SignupFormValues){
         console.log(data)
+        // todo dispatch and action and redirect after
         reset()
         // actions.resetForm()
     }
-
-    // function inputClasses(field:string): string{
-    //     return isFloating(field) ? 'floating' : ''
-    // }
-
-    // label should be floating as long as field isn't empty, or is focused (handled by the CSS)
-    // function isFloating(field: string  ){
-    //     return values[field as keyof typeof values]
-    // }
-   
-
     const CountryDatalist = useMemo(() =>
             <datalist id="country-codes" >
                 {countries.map( country => 
@@ -112,6 +78,7 @@ function Signup() {
                   </option>  
                 )}
             </datalist>, [countries])
+            console.log('form rendered')
   return (
     <main className='login window'>
         <ThemeToggle dispatch={dispatch} />
@@ -121,16 +88,26 @@ function Signup() {
             <b>Important:</b> There is no need for any detail here to be authentic!
             <br />
         </p>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            {/* {fields.map(field => <InputBox key={field.id} value={values[field.id as keyof typeof values]} handleChange={handleChange} handleBlur={handleBlur} data={field} isTouched={touched[field.id as keyof typeof touched]} error={errors[field.id as keyof typeof errors]} classes={inputClasses}/>)} */}
-            {fields.map((field) => { return (<InputBoxRHF key={field.id} register={register(field.id)} data={field} error={errors[field.id as keyof typeof errors]} isDirty={dirtyFields[field.id as keyof typeof dirtyFields]}/>)})}
-
-            <div className="divided-input-box">
-                {CountryDatalist}
-            {/* {phoneFields.map(field => <InputBox key={field.id} value={values[field.id as keyof typeof values]} handleChange={handleChange} handleBlur={handleBlur} data={field} isTouched={touched[field.id as keyof typeof touched]} error={errors[field.id as keyof typeof errors]} classes={inputClasses}/>)} */}
-            {/* {phoneFields.map((field, idx) => <InputBox key={field.id} {...inputBoxProps(field.id, idx, phoneFields)}/>)} */}
-
+        <form onSubmit={(e)=>handleSubmit(onSubmit)(e).catch((e)=>{
+            // todo: add error handling logic
+            console.log('serverside errors etc. logic should be here', e)})}>
+            {fields.map((field) => { return (
+                <div className={field.length > 1 ? 'divided-input-box' : 'full-input-box'}>
+                {field.map(data => {
+                    return (
+                        <InputBoxRHF 
+                        key={data.id} 
+                        data={data} 
+                        register={register(data.id as keyof SignupFormValues, {onBlur: () => {trigger(data.id as keyof SignupFormValues)}})} 
+                        error={errors[data.id as keyof typeof errors]} 
+                        isDirty={dirtyFields[data.id as keyof typeof dirtyFields]} 
+                        currVal={getValues(data.id as keyof SignupFormValues)}
+                        />
+                    )
+            })}
             </div>
+            )})}
+            {CountryDatalist}
             <button type="submit" className="call-to-action" data-theme="call-to-action">Sign up</button>
         </form>
         <p className="text" data-theme="text">Already have a user? <Link to='/'>Login page</Link></p>
